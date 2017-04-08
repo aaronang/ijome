697b4c8e12ce70485982b77b1d613e840de7defd
Aaron Ang <awz.ang@gmail.com>
2017-04-07 22:39:16 -0700

    Initialize ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "env": {
+    "node": true,
+    "es6": true,
+    "browser": true
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "strict": 0
+  },
+  "parserOptions": {
+    "sourceType": "module"
+  }
+}
