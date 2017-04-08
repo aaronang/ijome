21b14826b4c932ac0b840fea6e8e7da51ac0801e
Aaron Ang <awz.ang@gmail.com>
2017-04-08 11:53:33 -0700

    🗑  Clean up code

--- src/index.js
+++ src/index.js
@@ -1,88 +1,56 @@
-import {app, BrowserWindow, globalShortcut} from 'electron'
+import {app, BrowserWindow, globalShortcut, clipboard} from 'electron'
 import robot from 'robotjs'
 import path from 'path'
 import url from 'url'
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
-  // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600, frame: false})
 
-  // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
   createWindow()
 
   win.on('hide', () => {
-    robot.typeString("hello")
+    robot.keyTap("v", "command")
   })
 
-  const colon = globalShortcut.register(':', () => {
+  const colon = globalShortcut.register('Super+;', () => {
     app.focus()
-  })
-
-  const space = globalShortcut.register('Space', () => {
-    app.hide()
-  })
-
-  const esc = globalShortcut.register('Escape', () => {
-    app.hide()
   })
 
   if (!colon) {
     console.log('registration failed')
   }
+})
 
-  if (!space) {
-    console.log('Space registration failed')
-  }
-
-  if (!esc) {
-    console.error('Escape registration failed')
-  }
+app.on('focus', () => {
+  clipboard.writeText("✨")
 })
 
 app.on('will-quit', () => {
-  // Unregister all shortcuts.
   globalShortcut.unregisterAll()
 })
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
