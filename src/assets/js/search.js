084e5ae9f5a18d1fbd09661c99e69339142370e4
Aaron Ang <awz.ang@gmail.com>
2017-04-16 17:36:39 -0700

    👆  Allow clicking on emoji

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -5,16 +5,46 @@ const results = document.getElementById("results")
 const search = document.getElementById("search")
 let selected = null
 
+emojis.map(emoji).forEach(element => results.appendChild(element))
+results.firstElementChild.classList.add("selected")
+
+function mouseenter(e) {
+  if (selected !== null) {
+    selected.classList.remove("selected")
+  } else {
+    results.firstElementChild.classList.remove("selected")
+  }
+  selected = e.target
+  selected.classList.add("selected")
+  search.value = selected.lastChild.textContent
+}
+
+function click(e) {
+  selected = e.currentTarget
+  const emoji = selected.firstChild.textContent
+  ipcRenderer.send('finish', emoji)
+  selected.classList.remove("selected")
+  results.firstElementChild.classList.add("selected")
+  search.value = ""
+  selected = null
+}
+
 function emoji(data) {
   const div = document.createElement('div')
   div.className = "result"
   div.innerHTML = `<span class="emoji">${data.emoji}</span> <span class="description">${data.aliases[0]}</span>`
+  div.addEventListener('mouseenter', mouseenter)
+  div.addEventListener('click', click)
   return div
 }
 
-emojis.map(emoji).forEach(element => results.appendChild(element))
-
-results.firstElementChild.classList.add("selected")
+function filterEmojis() {
+  const query = search.value
+  console.log(query)
+  results.innerHTML = ""
+  emojis.filter(e => e.aliases[0].includes(query))
+      .forEach(element => results.appendChild(emoji(element)))
+}
 
 search.onkeydown = function(e) {
   if (e.keyCode === 9) { // Tab
@@ -50,12 +80,4 @@ search.onkeyup = function(e) {
     selected = results.firstElementChild
     selected.classList.add("selected")
   }
-}
-
-function filterEmojis() {
-  const query = search.value
-  console.log(query)
-  results.innerHTML = ""
-  emojis.filter(e => e.aliases[0].includes(query))
-      .forEach(element => results.appendChild(emoji(element)))
 }
